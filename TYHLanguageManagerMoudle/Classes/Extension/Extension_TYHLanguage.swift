77802ffce0a8785c163f196f0b3f62a3e26e2705
YaoHua Tan <yaohuatan@Shinkai.local>
2019-10-26 15:23:18 +0800

    first

---
+++ TYHLanguageManagerMoudle/Classes/Extension/Extension_TYHLanguage.swift
@@ -0,0 +1,13 @@
+//
+//  Extension_TYHLanguage.swift
+//  TYHLanguageManager
+//
+//  Created by YaoHua Tan on 2019/10/26.
+//  Copyright © 2019 YaoHua Tan. All rights reserved.
+//
+
+import UIKit
+
+class Extension_TYHLanguage: NSObject {
+
+}
